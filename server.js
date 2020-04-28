d5e0b4c66a0b40bae0b24805b8102002de7ef883
Tiago Andrade <tiagopgr22@gmail.com>
2020-04-28 08:40:35 -0300

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.options('*', cors())
+app.disable('x-powered-by');
 
 const mongoose = require('mongoose');
 const mongoOptions = {
@@ -12,12 +19,10 @@ require('./models/Information');
 mongoose.connect(process.env.MONGO_DATASOURCE, mongoOptions);
 const Information = mongoose.model('Information');
 
-//app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 app.get('/api/informations', async (req, res) => {
     const data = await Information.find().sort({ "timestamp": -1 });
-    console.log(data);
-    res.send(data);
+    res.json(data);
+    console.log(`Consulta realizada ${new Date()}`);
 });
 
 app.listen(process.env.PORT || 3000);
